7364980d9af74868b54d7da28670b27260331416
hexaquarks <seseriusca@gmail.com>
2024-01-06 17:13:42 -0500

    Added custom fetching functionality with custom deserialization mechanism

--- backend/src/fetch_lichess.rs
+++ backend/src/fetch_lichess.rs
@@ -1,80 +1,94 @@
 use crate::api::{ChessDataRequest, ChessDataResponse};
+use actix_web::Error;
+use futures_util::StreamExt;
+use reqwest;
+use serde_json::{self};
 
-use futures::stream::StreamExt;
-use lichess_api::client::LichessApi;
-use lichess_api::model::games::export::by_user::{GetQuery, GetRequest};
-use lichess_api::model::games::export::Base;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-use config::{Config, File};
-use serde_json;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GameJson {
+    pub clock: Option<Clock>,
+    pub clocks: Option<Vec<i64>>,
+    pub created_at: Option<u64>,
+    pub id: Option<String>,
+    pub last_move_at: Option<u64>,
+    pub moves: Option<String>,
+    pub perf: Option<String>,
+    pub players: Option<Players>,
+    pub rated: Option<bool>,
+    pub speed: Option<String>,
+    pub status: Option<String>,
+    pub variant: Option<String>,
+    pub winner: Option<String>,
+    #[serde(flatten)]
+    pub extra: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Clock {
+    pub increment: Option<i32>,
+    pub initial: Option<i32>,
+    pub total_time: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Players {
+    pub black: Option<PlayerDetail>,
+    pub white: Option<PlayerDetail>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerDetail {
+    pub rating: Option<i32>,
+    pub rating_diff: Option<i32>,
+    pub user: Option<User>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    pub id: Option<String>,
+    pub name: Option<String>,
+}
 
 pub async fn fetch_lichess_player_data(
     request_data: ChessDataRequest,
-) -> Result<ChessDataResponse, actix_web::Error> {
-    let mut settings = Config::default();
-    settings
-        .merge(File::with_name("keys"))
-        .expect("Failed to read keys file");
-
-    let api_key = settings
-        .get::<String>("api_key")
-        .expect("API key not found in keys file");
-
+) -> Result<ChessDataResponse, Error> {
     let client = reqwest::Client::new();
-    let api_token = Some(api_key); // Replace with actual token
-    let api = LichessApi::new(&client, api_token);
+    let url = format!(
+        "https://lichess.org/api/games/user/{}?max={}&rated=true&clocks=true",
+        request_data.username, request_data.games_count,
+    );
 
-    // Construct the GetQuery object
-    let base = Base {
-        moves: true,
-        pgn_in_json: true,
-        tags: true,
-        clocks: true,
-        evals: false,
-        accuracy: false,
-        opening: false,
-        literate: false,
-        players: None,
-    };
+    let res = client
+        .get(&url)
+        .header("Accept", "application/x-ndjson")
+        .send()
+        .await
+        .map_err(|e| {
+            actix_web::error::ErrorInternalServerError(format!("Request failed: {:?}", e))
+        })?;
 
-    let query = GetQuery {
-        base: base,
-        since: None,
-        until: None,
-        max: request_data.games_count as u64,
-        vs: None,
-        rated: Some(true),
-        perf_type: None,
-        color: None,
-        analysed: None,
-        ongoing: None,
-        finished: None,
-        last_fen: None,
-        sort: None,
-    };
-
-    let request = GetRequest::new(&request_data.username, query);
-
-    match api.export_games_of_user(request).await {
-        Ok(stream) => {
-            stream
-                .for_each(|game_result| async {
-                    match game_result {
-                        Ok(game) => {
-                            println!("{:#?}", game);
-                        }
-                        Err(e) => eprintln!("Error processing game: {:?}", e),
-                    }
-                })
-                .await;
-
-            Ok(ChessDataResponse { time: 14 })
-        }
-        Err(e) => {
-            eprintln!("Failed to fetch games: {:?}", e);
-            Err(actix_web::error::ErrorInternalServerError(
-                "Failed to fetch games",
-            ))
-        }
+    if !res.status().is_success() {
+        return Err(Error::from(actix_web::error::ErrorInternalServerError(
+            "Lichess API returned non-success status",
+        )));
     }
+
+    let stream = res.bytes_stream();
+    stream
+        .for_each(|game_result| async {
+            match game_result {
+                Ok(game) => {
+                    let a: GameJson =
+                        serde_json::from_slice(&game).expect("Failed to serialize GameJson");
+
+                    println!("moves: {}", a.moves.unwrap());
+                }
+                Err(e) => eprintln!("Error processing game: {:?}", e),
+            }
+        })
+        .await;
+    Ok(ChessDataResponse { time: 15 })
 }
