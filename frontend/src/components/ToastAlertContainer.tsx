0cc99c6c11bb275a4cba11decd097b5e5154625c
hexaquarks <seseriusca@gmail.com>
2024-02-18 15:32:39 -0500

    Use react-group-transition for scroll transitions on the stack of toast alerts

--- frontend/src/components/ToastAlertContainer.tsx
+++ frontend/src/components/ToastAlertContainer.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { ToastAlert, ToastAlertProps } from './ToastAlert';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 interface ToastAlertContainerProps {
     toasts: ToastAlertProps[];
@@ -8,9 +10,17 @@ interface ToastAlertContainerProps {
 const ToastAlertContainer: React.FC<ToastAlertContainerProps> = ({ toasts, removeToast }) => {
   return (
     <div className="fixed top-0 right-0 p-4 space-y-2" style={{ zIndex: 1000 }}>
-      {toasts.map((toast) => (
-        <ToastAlert key={toast.id} {...toast} removeToast={removeToast} />
-      ))}
+      <TransitionGroup>
+        {toasts.map((toast) => (
+          <CSSTransition
+            key={toast.id}
+            timeout={500}
+            classNames="toast"
+          >
+            <ToastAlert {...toast} removeToast={removeToast} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
