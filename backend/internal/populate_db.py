23677643f0a53f686b3ad5925d11552e04bec4cd
hexaquarks <seseriusca@gmail.com>
2024-07-11 15:02:27 -0400

    Make user elo an optional field in backend fetch endpoint

--- backend/internal/populate_db.py
+++ backend/internal/populate_db.py
@@ -1,19 +1,17 @@
-from ast import Continue
-from telnetlib import GA
 import requests
 import time, datetime
 from dataclasses import dataclass, field
 from typing import List, Tuple, Dict, Any, Set
 from enum import Enum
 
-BIND_ADDRESS: str = "http://localhost:8000/fetch-chess-data" 
-N_USERS_TO_PROCESS_FOR_GAME_MODE: int = 50
+BIND_ADDRESS: str = "http://localhost:8000/fetch-chess-data"
+N_USERS_TO_PROCESS_FOR_GAME_MODE: int = 5
 
 class GameMode(Enum):
     BLITZ = 'blitz'
     RAPID = 'rapid'
     BULLET = 'bullet'
-    
+
 class UserRatingPair:
     def __init__(self, user_name: str, elo_rating: int):
         self.user_name: str = user_name
@@ -22,7 +20,35 @@ class UserRatingPair:
     def __iter__(self):
         yield self.user_name
         yield self.elo_rating
-        
+
+@dataclass
+class ELOBraketsCache:
+    braket_info_impl: Dict[str, int] = field(default_factory=lambda: {
+        "0-400": 0,
+        "400-800": 0,
+        "800-1200": 0,
+        "1200-1600": 0,
+        "1600-2000": 0,
+        "2000-2400": 0,
+    })
+    
+    def add_data(self, elo_rating: int):
+        match elo_rating:
+            case _ if 0 <= elo_rating < 400:
+                self.braket_info_impl["0-400"] += 1
+            case _ if 400 <= elo_rating < 800:
+                self.braket_info_impl["400-800"] += 1
+            case _ if 800 <= elo_rating < 1200:
+                self.braket_info_impl["800-1200"] += 1
+            case _ if 1200 <= elo_rating < 1600:
+                self.braket_info_impl["1200-1600"] += 1
+            case _ if 1600 <= elo_rating < 2000:
+                self.braket_info_impl["1600-2000"] += 1
+            case _ if 2000 <= elo_rating < 2400:
+                self.braket_info_impl["2000-2400"] += 1
+            case _:
+                print(f"ELO rating {elo_rating} is out of defined brackets.")
+
 @dataclass
 class BackendRequestPayload:
     user_name: str
@@ -41,10 +67,10 @@ class BackendRequestPayload:
     @staticmethod
     def from_json(data: Dict[str, Any]) -> "BackendRequestPayload":
         return BackendRequestPayload(
-            user_name = data["username"],
-            games_count = data["games_count"],
-            game_mode = data["game_mode"],
-            user_color = data["user_color"]
+            user_name=data["username"],
+            games_count=data["games_count"],
+            game_mode=GameMode(data["game_mode"]),
+            user_color=data["user_color"]
         )
 
 @dataclass
@@ -55,12 +81,12 @@ class BackendResponse:
     @staticmethod
     def from_json(data: Dict[str, Any]) -> "BackendResponse":
         return BackendResponse(
-            average_time = data["time"],
-            opponents_and_their_rating = [
+            average_time=data["time"],
+            opponents_and_their_rating=[
                 UserRatingPair(opponent[0], opponent[1]) for opponent in data["players_considered"]
             ]
         )
-        
+
 class Processor:
     def __init__(self):
         self.results: List[List[BackendResponse]] = []
@@ -69,17 +95,17 @@ class Processor:
     def get_starting_user_rating_pairs() -> List[UserRatingPair]:
         return [
             UserRatingPair("Hexaquarks1", 1900),
-            UserRatingPair("fifthart", 1800) 
+            UserRatingPair("fifthart", 1800)
         ]
     
     class UserProcessor:
         def __init__(self, game_mode: GameMode):
             self.game_mode: GameMode = game_mode
-            
             self.local_results: List[BackendResponse] = []
+            self.elo_braket_info: ELOBraketsCache = ELOBraketsCache()
             
             # Set cache for usernames, to not process same user again
-            self.processed_users: Set[str] = set() 
+            self.processed_users: Set[str] = set()
             
             # Variables for statistical purposes
             self.nb_processed_users_for_curr_game_mode: int = 0
@@ -93,25 +119,25 @@ class Processor:
             user_color: str) -> BackendResponse:
             
             payload = BackendRequestPayload(
-                user_name = user_name,
-                games_count = games_count,
-                game_mode = game_mode,
-                user_color = user_color
+                user_name=user_name,
+                games_count=games_count,
+                game_mode=game_mode,
+                user_color=user_color
             )
             try:
                 response = requests.post(
                     BIND_ADDRESS, 
-                    headers = {
+                    headers={
                         "Content-Type": 'application/json',
                         "x-requested-by": "internal"
                     },
-                    json = payload.to_json())
-                response.raise_for_status() 
+                    json=payload.to_json())
+                #response.raise_for_status()
                 response_data = response.json()
                 
                 return BackendResponse.from_json(response_data)
             except Exception as e:
-                print(f"A server error occured while trying toprocess user {user_name}: {e}")
+                print(f"A server error occurred while trying to process user {user_name}: {e}")
                 raise
             
         def process_users_for_game_mode(self):
@@ -133,42 +159,62 @@ class Processor:
                 if user_name in self.processed_users:
                     continue 
                     
-                print(f"[{datetime.datetime.now()}] Game mode {self.game_mode} - processing user: {user_name}")
+                print(f"[{datetime.datetime.now()}] Game mode {self.game_mode.value} - processing user: {user_name}")
             
                 try: 
                     result: BackendResponse = self.process_user(user_name, 10, self.game_mode, "both")
                     self.local_results.append(result)
                     self.nb_processed_users_for_curr_game_mode += 1
                     
+                    # Update ELO brackets
+                    self.elo_braket_info.add_data(user_info.elo_rating)
+                    
                     user_rating_pairs.extend(result.opponents_and_their_rating)
-                    print(f"[{datetime.datetime.now()}] Game mode {self.game_mode} - Successfully processed user: {user_name}")
+                    print(f"[{datetime.datetime.now()}] Game mode {self.game_mode.value} - Successfully processed user: {user_name}")
                     
                 except Exception as e:
-                    # We don't really care what kind of error was returned from the server, I think.
-                    # Indeed, this procedure is for internal use only. Here I can afford to just
-                    # reject the erroneous user and add it to the processed users set
-                    
-                    print(f"[{datetime.datetime.now()}] Game mode {self.game_mode} - An error occurred while trying to process player {user_name}. It was skipped in the analysis")
+                    print(f"[{datetime.datetime.now()}] Game mode {self.game_mode.value} - An error occurred while trying to process player {user_name}. It was skipped in the analysis")
                 
                 self.processed_users.add(user_name)
                 
             end_time = time.time()
             total_time = end_time - start_time
             
-            print(f"Total execution time for game mode: {self.game_mode} is: {total_time:.2f} seconds")
-            print(f"===== Finished running game mode: {self.game_mode} =====")
+            print(f"Total execution time for game mode: {self.game_mode.value} is: {total_time:.2f} seconds")
+            print(f"===== Finished running game mode: {self.game_mode.value} =====")
+            
+        def print_table(self):
+            print(f"\n=== GAME MODE: {self.game_mode.value} ===")
+            processed_percentage = (self.nb_processed_users_for_curr_game_mode / self.nb_total_considered_users_for_curr_game_mode) * 100 if self.nb_total_considered_users_for_curr_game_mode > 0 else 0
+            print(f"Number of users processed: {self.nb_processed_users_for_curr_game_mode} out of {self.nb_total_considered_users_for_curr_game_mode} ({processed_percentage:.2f}%)\n")
+            
+            headers = ["[0-400]", "[400-800]", "[800-1200]", "[1200-1600]", "[1600-2000]", "[2000-2400]"]
+            values = [
+                self.elo_braket_info.braket_info_impl["0-400"],
+                self.elo_braket_info.braket_info_impl["400-800"],
+                self.elo_braket_info.braket_info_impl["800-1200"],
+                self.elo_braket_info.braket_info_impl["1200-1600"],
+                self.elo_braket_info.braket_info_impl["1600-2000"],
+                self.elo_braket_info.braket_info_impl["2000-2400"]
+            ]
+            
+            # Print the headers
+            print(" | ".join(f"{header:^12}" for header in headers))
+            print("-" * (13 * len(headers) - 1))
+            
+            # Print the values
+            print(" | ".join(f"{value:^12}" for value in values))
+
 
     def process_users_for_all_game_modes(self):
         print(f"=====  Starting script  =====")
         for game_mode in GameMode:
             userProcessor = self.UserProcessor(game_mode)
             userProcessor.process_users_for_game_mode()
-            
+            userProcessor.print_table()
             self.results.append(userProcessor.local_results)
             
         print(f"=====  Script finished running. Results:  =====")
-
-## Need to keep info on processed elo brakets
 
 if __name__ == "__main__":
     processor = Processor()
