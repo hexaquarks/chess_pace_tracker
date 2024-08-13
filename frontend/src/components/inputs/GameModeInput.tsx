614ceabfd1d5579ef96a53415e259a6a5b1b0d5a
hexaquarks <seseriusca@gmail.com>
2024-08-13 18:16:42 -0400

    Fix double drop down visual bug

--- frontend/src/components/inputs/GameModeInput.tsx
+++ frontend/src/components/inputs/GameModeInput.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
 interface GameModeInputProps {
-  value: string;
-  onChange: (value: string) => void;
+    value: string;
+    onChange: (value: string) => void;
 }
 
 const GameModeInput: React.FC<GameModeInputProps> = ({ value, onChange }) => (
-  <div>
-    <label className="block text-white text-xs xs:text-sm font-bold mb-2" htmlFor="game-mode">
-      Game mode
-    </label>
-    <select
-      className="appearance-auto bg-gray-800 text-sm xs:text-base border-none w-full text-white mr-3 py-1 px-2 -ml-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-      id="game-mode"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    >
-      <option value="bullet">Bullet</option>
-      <option value="blitz">Blitz</option>
-      <option value="rapid">Rapid</option>
-    </select>
-    <div className="border-b border-gray-500 mb-2"></div>
-  </div>
+    <div>
+        <label
+            className="block text-white text-xs xs:text-sm font-bold mb-2"
+            htmlFor="game-mode"
+        >
+            Game mode
+        </label>
+        <select
+            className="appearance-none pl-3 bg-gray-800 text-sm xs:text-base border-none w-full text-white mr-3 py-1 px-2 -ml-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            id="game-mode"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        >
+            <option value="bullet">Bullet</option>
+            <option value="blitz">Blitz</option>
+            <option value="rapid">Rapid</option>
+        </select>
+        <div className="border-b border-gray-500 mb-2"></div> {/*This is for the bottom bar*/}
+    </div>
 );
 
 export default GameModeInput;
--- frontend/src/components/inputs/UserColorInput.tsx
+++ frontend/src/components/inputs/UserColorInput.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
 interface UserColorInputProps {
-  value: string;
-  onChange: (value: string) => void;
+    value: string;
+    onChange: (value: string) => void;
 }
 
 const UserColorInput: React.FC<UserColorInputProps> = ({ value, onChange }) => (
-  <div>
-    <label className="block text-white text-xs xs:text-sm font-bold mb-2" htmlFor="user-color">
-      User color
-    </label>
-    <select
-      className="appearance-auto bg-gray-800 text-sm xs:text-base border-none w-full text-white mr-3 py-1 px-2 -ml-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-      id="user-color"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    >
-      <option value="both">Both</option>
-      <option value="white">White</option>
-      <option value="black">Black</option>
-    </select>
-    <div className="border-b border-gray-500 mb-2"></div>
-  </div>
+    <div>
+        <label
+            className="block text-white text-xs xs:text-sm font-bold mb-2"
+            htmlFor="user-color"
+        >
+            User color
+        </label>
+        <select
+            className="appearance-none pl-3 bg-gray-800 text-sm xs:text-base border-none w-full text-white mr-3 py-1 px-2 -ml-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            id="user-color"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        >
+            <option value="both">Both</option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
+        </select>
+        <div className="border-b border-gray-500 mb-2"></div> {/*For bottom bar*/}
+    </div>
 );
 
 export default UserColorInput;
